311ea507f509678d56eeb6543f3289d9f6ee2518
Ropsenshtils <ropsenshtils@gmail.com>
2023-05-09 14:59:55 +0300

    add class Contact

--- fixture/application.py
+++ fixture/application.py
@@ -18,22 +18,6 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
-    def create_contact(self, Contact):
-        wd = self.wd
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill contact firm
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(Contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
-        # submit contact creation
-        wd.find_element_by_name("submit").click()
 
     def open_home_page(self):
         wd = self.wd
