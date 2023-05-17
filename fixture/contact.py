cb712f05afbd622f63e31ebbe06ce61fbbec2f9c
Ropsenshtils <ropsenshtils@gmail.com>
2023-05-17 17:34:43 +0300

    added test_del_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
 class ContactHelper:
     def __init__(self, app):
         self.app = app
@@ -18,3 +21,11 @@ class ContactHelper:
         wd.find_element_by_name("lastname").send_keys(contact.lastname)
         # submit contact creation
         wd.find_element_by_name("submit").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # Select first contact
+        wd.find_element_by_name("selected[]").click()
+        # Submit first contact
+        wd.find_element(By.XPATH, '//input[@value="Delete"]').click()
+        # wd.find_element_by_xpath('//input[@value="Delete"]')
