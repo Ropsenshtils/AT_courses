c4a4d85a8f8da60336bbb5aa4925cff46dae6597
Ropsenshtils <ropsenshtils@gmail.com>
2023-05-21 23:57:56 +0300

    added random index t0 test_compare_home_page_and_edit #14 fix

--- tests/test_check_contact.py
+++ tests/test_check_contact.py
@@ -1,9 +1,10 @@
 import re
-
+from random import randrange
 
 def test_compare_home_page_and_edit(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_info_from_edit_page(0)
+    index = randrange(len(app.contact.get_contact_list()))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_info_from_edit_page(index)
     assert contact_from_home_page.id == contact_from_edit_page.id
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
