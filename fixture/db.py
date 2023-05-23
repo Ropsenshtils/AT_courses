8eb709a00637c06bf80eb629cfce618ea700ac57
Ropsenshtils <ropsenshtils@gmail.com>
2023-05-23 08:50:19 +0300

    contacts test_compare_home_page_and_db #21

--- fixture/db.py
+++ fixture/db.py
@@ -16,7 +16,7 @@ class DbFixture:
                                           autocommit=True)
 
     def get_group_list(self):
-        list=[]
+        list = []
         cursor = self.connection.cursor()
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
@@ -28,13 +28,16 @@ class DbFixture:
         return list
 
     def get_contact_list(self):
-        list=[]
+        list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated ='0000-00-00 00:00:00'")
+            cursor.execute(
+                "select id, firstname, lastname, home, mobile, work, address, email, email2, email3   from addressbook where deprecated ='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, home, mobile, work, address, email1, email2, email3) = row
+                list.append(
+                    Contact(id=str(id), firstname=firstname, lastname=lastname, homephone=home, mobilephone=mobile,
+                            workphone=work, all_addresses=address, email1=email1, email2=email2, email3=email3))
         finally:
             cursor.close()
         return list
--- tests/test_check_contact.py
+++ tests/test_check_contact.py
@@ -1,5 +1,7 @@
 import re
 from random import randrange
+from model.contact import Contact
+
 
 def test_compare_home_page_and_edit(app):
     index = randrange(len(app.contact.get_contact_list()))
@@ -11,6 +13,21 @@ def test_compare_home_page_and_edit(app):
     assert contact_from_home_page.all_addresses == contact_from_edit_page.all_addresses
     assert contact_from_home_page.all_emails == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones == merge_phone_like_on_home_page(contact_from_edit_page)
+
+
+def test_compare_home_page_and_db(app, db):
+    contacts_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contacts_ui = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    emails = list(map(merge_emails_like_on_home_page, contacts_db))
+    phones = list(map(merge_phone_like_on_home_page, contacts_db))
+    addresses = list(map(addresses_like_on_home_page, contacts_db))
+    for i in range(len(contacts_db)):
+        assert contacts_ui[i].id == contacts_db[i].id
+        assert contacts_ui[i].lastname == contacts_db[i].lastname
+        assert contacts_ui[i].firstname == contacts_db[i].firstname
+        assert contacts_ui[i].all_addresses == addresses[i]
+        assert contacts_ui[i].all_emails == emails[i]
+        assert contacts_ui[i].all_phones == phones[i]
 
 
 def clear(s):
@@ -26,5 +43,9 @@ def merge_phone_like_on_home_page(contact):
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                                filter(lambda x: x is not None, [contact.email1, contact.email2,
-                                                                 contact.email3])))
+                            filter(lambda x: x is not None, [contact.email1, contact.email2,
+                                                             contact.email3])))
+
+
+def addresses_like_on_home_page(contact):
+    return re.sub("\r", "", contact.all_addresses)
